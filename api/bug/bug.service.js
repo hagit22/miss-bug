e83d81526aa2675c7c4cdbc1c3eeec9cda5ca9c1
hagit <hagit@ornim.com>
2024-02-23 05:30:33 +0200

    with mongo-DB - Bug-service update to Mongo

--- api/bug/bug.service.js
+++ api/bug/bug.service.js
@@ -1,5 +1,8 @@
 import { utilService } from '../../services/util.service.js'
 import { loggerService } from '../../services/logger.service.js'
+import { dbService } from '../../services/db.service.js';
+import { ObjectId } from 'mongodb';
+
 
 export const bugService = {
     query,
@@ -8,37 +11,32 @@ export const bugService = {
     save,
 }
 
-const BUG_FILE_NAME = './data/bug.json'
-var bugs = utilService.readJsonFile(BUG_FILE_NAME)
+const COLLECTION = 'bug'
 const PAGE_SIZE = 3
 
 async function query(filterBy = {}, sortObj = {}) {
     try {
-        let filteredBugs = [...bugs]
-        if (filterBy.title) {
-            const regExp = new RegExp(filterBy.title, 'i')
-            filteredBugs = filteredBugs.filter(bug => regExp.test(bug.title))
-        }
-        if (filterBy.minSeverity) {
-            filteredBugs = filteredBugs.filter(bug => bug.severity >= filterBy.minSeverity)
-        }
-        if (filterBy.label) {
-            filteredBugs = filteredBugs.filter(bug => bug.labels && bug.labels.includes(filterBy.label))
-        }
+        const collection = await dbService.getCollection(COLLECTION)
+
+        const criteria = _buildCriteria(filterBy)
+        const bugCursor = await collection.find(criteria)
+
         if (filterBy.pageIdx !== undefined) {
-            const startIdx = filterBy.pageIdx * PAGE_SIZE
-            filteredBugs = filteredBugs.slice(startIdx, startIdx + PAGE_SIZE)
-        }
-        if (filterBy.userId) {
-            filteredBugs = filteredBugs.filter(bug => bug.creator._id == filterBy.userId)
+            let pageIdx = filterBy.pageIdx < 0 ? 0 : filterBy.pageIdx
+            const lastDoc = collection.countDocuments - 1
+            pageIdx = pageIdx > lastDoc ? lastDoc : pageIdx
+
+            const startIdx = pageIdx * PAGE_SIZE
+            bugCursor.skip(startIdx).limit(PAGE_SIZE)
         }
         if (sortObj.sortBy !== '') {
-            const sortKey = sortObj.sortBy;
+            const sortKey = sortObj.sortBy != 'createdAt' ? sortObj.sortBy : '_id'
             const sortDir = sortObj.isAscending == "true" ? 1 : -1
-            filteredBugs = filteredBugs.sort((a,b) => 
-                (utilService.toLowerCaseAllowEmpty(a[sortKey]) > utilService.toLowerCaseAllowEmpty(b[sortKey]) ? 1 : -1) * sortDir);
+            bugCursor.collation({locale: "en" }).sort({[sortKey]: sortDir})
         }
-        return filteredBugs
+    
+        const bugs = bugCursor.toArray()
+        return bugs
     } 
     catch(err) {
         loggerService.error(`Had problems getting bugs...`)
@@ -48,7 +46,8 @@ async function query(filterBy = {}, sortObj = {}) {
 
 async function getById(bugId) {
     try {
-        const bug = bugs.find(bug => bug._id === bugId)
+        const collection = await dbService.getCollection(COLLECTION)
+        const bug = await collection.findOne({ _id: new ObjectId(bugId) })
         return bug
     } 
     catch (err) {
@@ -59,13 +58,14 @@ async function getById(bugId) {
 
 async function remove(bugId, loggedinUser) {
     try {
-        const index = bugs.findIndex(bug => bug._id === bugId)
-        const bug = bugs[index]
+        const collection = await dbService.getCollection(COLLECTION)
+        const bug = await collection.findOne({ _id: new ObjectId(bugId) })
+
         if (!loggedinUser.isAdmin && bug.creator._id !== loggedinUser._id) 
             throw ( 'You are not bug creator' )
-        bugs.splice(index, 1)
-        utilService.saveEntitiesToFile(bugs, BUG_FILE_NAME)
-        return `Bug ${bugId} removed`
+
+        const { acknowledged } = await collection.deleteOne({ _id: new ObjectId(bugId) })
+        return acknowledged ? `Bug ${bugId} removed` : `Did not remove bug ${bugId}`
     } 
     catch (err) {
         loggerService.error(`Had problems removing bug ${bugId}...`)
@@ -75,27 +75,44 @@ async function remove(bugId, loggedinUser) {
 
 async function save(bugToSave, loggedinUser) {
     try {
+        const collection = await dbService.getCollection(COLLECTION)
         if(bugToSave._id && bugToSave._id.length > 0) {
-            const index = bugs.findIndex(bug => bug._id === bugToSave._id)
-            if(index === -1) 
-                throw 'Bad Id'
-            //bugToSave.createdAt = new Date(Date.now()).toISOString()
-            const bug = bugs[index]
-            if (!loggedinUser.isAdmin && bug.creator._id !== loggedinUser?._id) 
+            if (!loggedinUser.isAdmin && bugToSave.creator._id !== loggedinUser?._id) 
                 throw ('You are not bug creator')
-            bugs.splice(index, 1, bugToSave)
-        } 
+            const bugUpdate = {
+                title: bugToSave.title,
+                severity: bugToSave.severity,
+                description: bugToSave.description,
+            }
+            const { acknowledged } = await collection.updateOne({ _id: new ObjectId(bugToSave._id) }, { $set: bugUpdate })
+            return acknowledged ? bugToSave : `Did not update bug`
+         } 
         else {
-            bugToSave._id = utilService.makeId()
-            bugToSave.createdAt = new Date(Date.now()).toISOString()
-            bugToSave.creator = { _id: loggedinUser._id, fullname: loggedinUser.fullname }
-            bugs.push(bugToSave)
+            bugToSave.creator = {_id: loggedinUser._id, fullname:  loggedinUser.fullname}
+            const { acknowledged } = await collection.insertOne(bugToSave)
+            return acknowledged ? bugToSave : `Did add bug`
         }
-        utilService.saveEntitiesToFile(bugs, BUG_FILE_NAME)
     } catch (err) {
         loggerService.error(`Had problems saving bug ${bugToSave._id}...`)
         throw err
     }
-    return bugToSave
+}
+
+function _buildCriteria(filterBy) {
+    const criteria = {}
+
+    if (filterBy.title) {
+        criteria.title = { $regex: filterBy.title, $options: 'i' }
+    }
+
+    if (filterBy.minSeverity) {
+        criteria.severity = { $gt: filterBy.minSeverity }
+    }
+
+    /*if (filterBy.label) {
+        criteria.labels = { $in: filterBy.label }
+    }*/
+
+    return criteria
 }
 
