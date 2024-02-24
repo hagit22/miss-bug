753d8631a2d4a85ab69cceba0f8133c6368241fa
hagit <hagit@ornim.com>
2024-02-24 15:55:57 +0200

    updated to atlas connection string

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 export default {
     //dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://hagity22:1234@cluster0.qbbrn5d.mongodb.net/test?retryWrites=true&w=majority',
     dbName: 'missbug_db'
 }
